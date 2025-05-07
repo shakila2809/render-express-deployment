64ae1e52f42d04bde6a0b633c67deedd48e74340
praveenkumar <praveen884444@gmail.com>
2025-05-07 11:40:31 +0530

    Your commit message

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/',(req,res)=>{
+    res.send({hi:'there'});
+
+})
+app.listen(port,()=>{
+    console.log("server started on port 8080")
+})
