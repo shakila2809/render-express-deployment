b44e8e3cbd0bc1e929e652a488d2cc6ae2f0b01d
praveenkumar <praveen884444@gmail.com>
2025-05-08 12:28:40 +0530

    'deplycheck'

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/',(req,res)=>{
-    res.send({hi:'there'});
-
+    res.send({hi:'there',hello:'there'});
 })
 app.listen(port,()=>{
     console.log("server started on port 8080")
